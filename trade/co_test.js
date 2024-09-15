1ee0716c4d77df7a72d1f7b04e60ffe29ce22f5f
adamant-al <adamantim@yandex.ru>
2024-09-15 21:23:25 +0400

    fix: azbit connector

--- trade/co_test.js
+++ trade/co_test.js
@@ -20,6 +20,25 @@ module.exports = {
   async test() {
     console.log('==========================');
 
+
+    // const testOrderPrice = 0.02971; // Setting a limit price, assuming the current highest bid and lowest ask are 3500–3505. Adjust it according to the current price to place an order in the spread.
+    // const testOrderMarket = 'ADM/USDT';
+
+    // const partFilledOrder = await traderapi.placeOrder('sell', testOrderMarket, testOrderPrice, 10, 1, undefined);
+    // const filledOrder2 = await traderapi.placeOrder('buy', testOrderMarket, testOrderPrice, 5, 1, undefined);
+
+    // console.log(await traderapi.getOpenOrders(testOrderMarket));
+    // console.log(await traderapi.getOrderDetails(filledOrder2.orderId, testOrderMarket)); // Details for the filled order
+    // console.log(await traderapi.getOrderDetails(partFilledOrder.orderId, testOrderMarket)); // Details for the part_filled order
+
+    // Testing getting non-existent order details
+    // console.log(await traderapi.getOrderDetails('146d61be-9841-46c6-997c-28e20df503b4', testOrderMarket));
+    // console.log(await traderapi.getOrderDetails('119353984789', testOrderMarket));
+    // console.log(await traderapi.getOrderDetails('65707c1285a72b0007ee2cbd2', testOrderMarket));
+    // console.log(await traderapi.getOrderDetails('123-any-order-number', testOrderMarket));
+    // console.log(await traderapi.getOrderDetails(undefined, testOrderMarket));
+    
+    
     // const { ordersDb } = db;
     // const order = await ordersDb.findOne({
     //   _id: 'orderId',
@@ -36,10 +55,19 @@ module.exports = {
     // const req = await traderapi.getTradesHistory('eth/usdt');
     // console.log(req);
 
-    // setTimeout(() => {
-    //   const traderapi = require('./trader_' + 'azbit')(config.apikey, config.apisecret, config.apipassword, log);
-    //   console.log(require('./orderUtils').parseMarket('ADM/USDT', 'azbit'));
-    // }, 3000);
+    // setTimeout(async () => {
+    //   console.log('100');
+    //   console.log(await traderapi.getOrderDetails(filledOrder2.orderId, testOrderMarket)); // Details for the filled order
+    //   console.log(await traderapi.getOrderDetails(partFilledOrder.orderId, testOrderMarket)); // Details for the part_filled order
+    //   console.log('100-end');
+    // }, 100);
+
+    // setTimeout(async () => {
+    //   console.log('300');
+    //   console.log(await traderapi.getOrderDetails(filledOrder2.orderId, testOrderMarket)); // Details for the filled order
+    //   console.log(await traderapi.getOrderDetails(partFilledOrder.orderId, testOrderMarket)); // Details for the part_filled order
+    //   console.log('300-end');
+    // }, 300);
 
     // const orderCollector = require('./orderCollector');
     // const cancellation = await orderCollector.clearOrderById(
@@ -47,7 +75,7 @@ module.exports = {
     // console.log(cancellation);
 
     // console.log(await traderapi.cancelAllOrders('BNB/USDT'));
-    // console.log(await traderapi.cancelOrder('5d13f3e8-dcb3-4a6d-88c1-16cf6e8d8179', undefined, 'DOGE/USDT'));
+    // console.log(await traderapi.cancelOrder('5b9e5590-2e34-4a1c-93a8-4aa129fd96bc', undefined, 'ADM/USDT'));
     // console.log(await traderapi.cancelOrder('ODM54B-5CJUX-RSUKCK', undefined, 'DOGE/USDT'));
     // console.log(traderapi.features().orderNumberLimit);
   },
